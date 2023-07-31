41e8a8ef03195e80e940ba5504dab8a5cf98c4b1
001daemon <139598345+001daemon@users.noreply.github.com>
2023-07-31 11:57:42 -0400

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+def install_application(github_url):
+    app_name = "PySimpleLogger"  # Replace with the name of the application
+
+    try:
+        # Clone the GitHub repository
+        subprocess.check_call(["git", "clone", github_url])
+        print(f"Repository cloned successfully.")
+
+        # Change directory to the cloned repository
+        os.chdir(app_name)
+
+        # Follow the installation instructions for the application
+        # You might need to run specific commands based on the repository's structure
+        
+	# Assuming the Python script to run is named "main.py"
+        subprocess.check_call(["python3", "LiSimpleLogger.py"])
+        
+        print(f"{app_name} has been installed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while installing {app_name}: {e}")
+
+if __name__ == "__main__":
+    github_url = "https://github.com/hemantapkh/PySimpleLogger/"  # Replace with the actual GitHub repository link
+    install_application(github_url)
